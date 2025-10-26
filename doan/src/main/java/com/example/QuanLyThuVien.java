412c1395966ece5457c7c3d59500ed578a325e2b
Tuan <3124410393@sv.sgu.edu.vn>
2025-10-26 09:56:05 +0700

    Update quanythuvien muc 1,2

--- doan/src/main/java/com/example/QuanLyThuVien.java
+++ doan/src/main/java/com/example/QuanLyThuVien.java
@@ -67,6 +67,10 @@ public class QuanLyThuVien{
         dsnv.docFile();
         dsphieuphat.docFile(dsquydinhphat);
         dsquydinhphat.docFile();
+        dstg.DocFileTacGia("src/main/java/com/example/TacGia.txt");
+        dss.DocFileSach("src/main/java/com/example/Sach.txt");
+        dstl.DocFileTheLoai("src/main/java/com/example/TheLoai.txt");
+        dsnxb.DocFileNhaXuatBan("src/main/java/com/example/NhaXuatBan.txt");
     }
     public void menu(){
         int choice;
@@ -95,10 +99,10 @@ public class QuanLyThuVien{
             }
             switch(choice){
                 case 1:
-                   menuSach();
+                   menuSach(dss);
                    break;
                 case 2:
-                    menuTacGia();
+                    menuTacGia(dstg);
                     break;
                 case 3:
                     menuNhanVien();
@@ -107,10 +111,10 @@ public class QuanLyThuVien{
                     menuDocGia();
                     break;
                 case 5:
-                    menuTheLoai();
+                    menuTheLoai(dstl);
                     break;
                 case 6:
-                    menuNhaXuatBan();
+                    menuNhaXuatBan(dsnxb);
                     break;
                 case 7:
                     menuPhieuMuon();
@@ -388,6 +392,126 @@ public void menuPhieuPhat(){
                 break;
             }
         } while (choice != 0);
+    }
+    public static void menuSach(DanhSachSach dss) {
+        Scanner sc = new Scanner(System.in);
+        int c;
+        do {
+            System.out.println("----- QUAN LY SACH -----");
+            System.out.println("1. Xem danh sach sach");
+            System.out.println("2. Them sach moi");
+            System.out.println("3. Sua thong tin sach");
+            System.out.println("4. Xoa sach");
+            System.out.println("5. Tim sach");
+            System.out.println("6. Doc file sach");
+            System.out.println("7. Ghi file sach");
+            System.out.println("0. Quay lai");
+            System.out.print("Nhap lua chon: ");
+            c = sc.nextInt();
+            sc.nextLine();
+
+            switch(c) {
+                case 1: dss.XemSach(); break;
+                case 2: dss.ThemSach(); break;
+                case 3: dss.SuaSach(); break;
+                case 4: dss.XoaSach(); break;
+                case 5: dss.TimSach(); break;
+                case 6: dss.DocFileSach("src/main/java/com/example/Sach.txt"); break;
+                case 7: dss.GhiFileSach("src/main/java/com/example/Sach.txt"); break;
+                case 0: System.out.println("Quay lai menu chinh..."); break;
+                default: System.out.println("Lua chon khong hop le!");
+            }
+        } while(c != 0);
+    }
+    public static void menuTheLoai(DanhSachTheLoai dstl) {
+        Scanner sc = new Scanner(System.in);
+        int c;
+        do {
+            System.out.println("----- QUAN LY THE LOAI-----");
+            System.out.println("1. Xem danh sach the loai");
+            System.out.println("2. Them the loai moi");
+            System.out.println("3. Sua thong tin the loai");
+            System.out.println("4. Xoa the loai");
+            System.out.println("5. Tim the loai");
+            System.out.println("6. Doc file the loai");
+            System.out.println("7. Ghi file the loai");
+            System.out.println("0. Quay lai");
+            System.out.print("Nhap lua chon: ");
+            c = sc.nextInt();
+            sc.nextLine();
+
+            switch(c) {
+                case 1: dstl.XemTacGia(); break;
+                case 2: dstl.ThemTheLoai(); break;
+                case 3: dstl.SuaTheLoai(); break;
+                case 4: dstl.XoaTheLoai(); break;
+                case 5: dstl.TimTheLoai(); break;
+                case 6: dstl.DocFileTheLoai("src/main/java/com/example/TheLoai.txt");
+                case 7: dstl.GhiFileTheLoai("src/main/java/com/example/TheLoai.txt"); break;
+                case 0: break;
+                default: System.out.println("Lua chon khong hop le!");
+            }
+        } while(c != 0);
+    }
+    public static void menuNhaXuatBan(DanhSachNhaXuatBan dsnxb) {
+        Scanner sc = new Scanner(System.in);
+        int c;
+        do {
+            System.out.println("----- QUAN LY NHA XUAT BAN-----");
+            System.out.println("1. Xem danh sach nha xuat ban");
+            System.out.println("2. Them nha xuat ban moi");
+            System.out.println("3. Sua thong tin nha xuat ban");
+            System.out.println("4. Xoa nha xuat ban");
+            System.out.println("5. Tim nha xuat ban");
+            System.out.println("6. Doc file nha xuat ban");
+            System.out.println("7. Ghi file nha xuat ban");
+            System.out.println("0. Quay lai");
+            System.out.print("Nhap lua chon: ");
+            c = sc.nextInt();
+            sc.nextLine();
+
+            switch(c) {
+                case 1: dsnxb.XemNXB(); break;
+                case 2: dsnxb.ThemNhaXuatBan(); break;
+                case 3: dsnxb.SuaNhaXuatBan(); break;
+                case 4: dsnxb.XoaNhaXuatBan(); break;
+                case 5: dsnxb.TimNhaXuatBan(); break;
+                case 6: dsnxb.DocFileNhaXuatBan("src/main/java/com/example/NhaXuatBan.txt"); break;
+                case 7: dsnxb.GhiFileNhaXuatBan("src/main/java/com/example/NhaXuatBan.txt"); break;
+                case 0: break;
+                default: System.out.println("Lua chon khong hop le!");
+            }
+        } while(c != 0);
+    }
+    public static void menuTacGia(DanhSachTacGia dstg) {
+        Scanner sc = new Scanner(System.in);
+        int c;
+        do {
+            System.out.println("----- QUAN LY TAC GIA -----");
+            System.out.println("1. Xem danh sach tac gia");
+            System.out.println("2. Them tac gia moi");
+            System.out.println("3. Sua thong tin tac gia");
+            System.out.println("4. Xoa tac gia");
+            System.out.println("5. Tim tac gia");
+            System.out.println("6. Doc file tac gia");
+            System.out.println("7. Ghi file tac gia");
+            System.out.println("0. Quay lai");
+            System.out.print("Nhap lua chon: ");
+            c = sc.nextInt();
+            sc.nextLine();
+
+            switch(c) {
+                case 1: dstg.XemTacGia(); break;
+                case 2: dstg.ThemTacGia(); break;
+                case 3: dstg.SuaTacGia(); break;
+                case 4: dstg.XoaTacGia(); break;
+                case 5: dstg.TimTacGia(); break;
+                case 6: dstg.DocFileTacGia("src/main/java/com/example/TacGia.txt"); break;
+                case 7: dstg.GhiFileTacGia("src/main/java/com/example/TacGia.txt"); break;
+                case 0: break;
+                default: System.out.println("Lua chon khong hop le!");
+            }
+        } while(c != 0);
     }
     public static void main(String[] args) {
     QuanLyThuVien ql = new QuanLyThuVien();
